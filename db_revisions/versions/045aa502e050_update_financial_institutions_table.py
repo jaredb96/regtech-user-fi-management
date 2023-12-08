8ab4c6e529cfe3fd5a44dbe7f4a5e7620f0a4d44
Nargis Sultani <nargis.sultani@cfpb.gov>
2023-12-08 10:40:07 -0500

    Removed the table_exists condition from updating financial institution alembic script

--- db_revisions/versions/045aa502e050_update_financial_institutions_table.py
+++ db_revisions/versions/045aa502e050_update_financial_institutions_table.py
@@ -10,10 +10,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
-from db_revisions.utils import table_exists
-
-
 # revision identifiers, used by Alembic.
 revision: str = "045aa502e050"
 down_revision: Union[str, None] = "549c612bf1c9"
@@ -22,72 +18,71 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    if table_exists("financial_institutions"):
-        op.add_column("financial_institutions", sa.Column("tax_id", sa.String(length=9), nullable=True))
-        op.add_column("financial_institutions", sa.Column("rssd_id", sa.Integer(), nullable=True))
-        op.create_unique_constraint("unique_association", "financial_institutions", ["tax_id", "rssd_id"])
-        op.add_column(
-            "financial_institutions", sa.Column("primary_federal_regulator_id", sa.String(length=4), nullable=True)
-        )
-        op.add_column("financial_institutions", sa.Column("hmda_institution_type_id", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("sbl_institution_type_id", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("hq_address_street_1", sa.String(), nullable=False))
-        op.add_column("financial_institutions", sa.Column("hq_address_street_2", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("hq_address_city", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("hq_address_state", sa.String(length=2), nullable=True))
-        op.add_column("financial_institutions", sa.Column("hq_address_zip", sa.String(length=5), nullable=False))
-        op.add_column("financial_institutions", sa.Column("parent_lei", sa.String(length=20), nullable=True))
-        op.add_column("financial_institutions", sa.Column("parent_legal_name", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("parent_rssd_id", sa.Integer(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("top_holder_lei", sa.String(length=20), nullable=True))
-        op.add_column("financial_institutions", sa.Column("top_holder_legal_name", sa.String(), nullable=True))
-        op.add_column("financial_institutions", sa.Column("top_holder_rssd_id", sa.Integer(), nullable=True))
-        op.create_index(
-            op.f("ix_financial_institutions_hmda_institution_type_id"),
-            "financial_institutions",
-            ["hmda_institution_type_id"],
-            unique=False,
-        )
-        op.create_index(
-            op.f("ix_financial_institutions_hq_address_state"),
-            "financial_institutions",
-            ["hq_address_state"],
-            unique=False,
-        )
-        op.create_index(
-            op.f("ix_financial_institutions_primary_federal_regulator_id"),
-            "financial_institutions",
+    op.add_column("financial_institutions", sa.Column("tax_id", sa.String(length=9), nullable=True))
+    op.add_column("financial_institutions", sa.Column("rssd_id", sa.Integer(), nullable=True))
+    op.create_unique_constraint("unique_association", "financial_institutions", ["tax_id", "rssd_id"])
+    op.add_column(
+        "financial_institutions", sa.Column("primary_federal_regulator_id", sa.String(length=4), nullable=True)
+    )
+    op.add_column("financial_institutions", sa.Column("hmda_institution_type_id", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("sbl_institution_type_id", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("hq_address_street_1", sa.String(), nullable=False))
+    op.add_column("financial_institutions", sa.Column("hq_address_street_2", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("hq_address_city", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("hq_address_state", sa.String(length=2), nullable=True))
+    op.add_column("financial_institutions", sa.Column("hq_address_zip", sa.String(length=5), nullable=False))
+    op.add_column("financial_institutions", sa.Column("parent_lei", sa.String(length=20), nullable=True))
+    op.add_column("financial_institutions", sa.Column("parent_legal_name", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("parent_rssd_id", sa.Integer(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("top_holder_lei", sa.String(length=20), nullable=True))
+    op.add_column("financial_institutions", sa.Column("top_holder_legal_name", sa.String(), nullable=True))
+    op.add_column("financial_institutions", sa.Column("top_holder_rssd_id", sa.Integer(), nullable=True))
+    op.create_index(
+        op.f("ix_financial_institutions_hmda_institution_type_id"),
+        "financial_institutions",
+        ["hmda_institution_type_id"],
+        unique=False,
+    )
+    op.create_index(
+        op.f("ix_financial_institutions_hq_address_state"),
+        "financial_institutions",
+        ["hq_address_state"],
+        unique=False,
+    )
+    op.create_index(
+        op.f("ix_financial_institutions_primary_federal_regulator_id"),
+        "financial_institutions",
+        ["primary_federal_regulator_id"],
+        unique=False,
+    )
+    op.create_index(
+        op.f("ix_financial_institutions_sbl_institution_type_id"),
+        "financial_institutions",
+        ["sbl_institution_type_id"],
+        unique=False,
+    )
+    with op.batch_alter_table("financial_institutions") as batch_op:
+        batch_op.create_foreign_key(
+            "fk_federal_regulator_financial_institutions",
+            "federal_regulator",
             ["primary_federal_regulator_id"],
-            unique=False,
+            ["id"],
         )
-        op.create_index(
-            op.f("ix_financial_institutions_sbl_institution_type_id"),
-            "financial_institutions",
+        batch_op.create_foreign_key(
+            "fk_address_state_financial_institutions", "address_state", ["hq_address_state"], ["code"]
+        )
+        batch_op.create_foreign_key(
+            "fk_hmda_institution_type_financial_institutions",
+            "hmda_institution_type",
+            ["hmda_institution_type_id"],
+            ["id"],
+        )
+        batch_op.create_foreign_key(
+            "fk_sbl_institution_type_financial_institutions",
+            "sbl_institution_type",
             ["sbl_institution_type_id"],
-            unique=False,
+            ["id"],
         )
-        with op.batch_alter_table("financial_institutions") as batch_op:
-            batch_op.create_foreign_key(
-                "fk_federal_regulator_financial_institutions",
-                "federal_regulator",
-                ["primary_federal_regulator_id"],
-                ["id"],
-            )
-            batch_op.create_foreign_key(
-                "fk_address_state_financial_institutions", "address_state", ["hq_address_state"], ["code"]
-            )
-            batch_op.create_foreign_key(
-                "fk_hmda_institution_type_financial_institutions",
-                "hmda_institution_type",
-                ["hmda_institution_type_id"],
-                ["id"],
-            )
-            batch_op.create_foreign_key(
-                "fk_sbl_institution_type_financial_institutions",
-                "sbl_institution_type",
-                ["sbl_institution_type_id"],
-                ["id"],
-            )
 
 
 def downgrade() -> None:
